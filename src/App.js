8d3dab5c879012550e6e57434aa03011363cc4f2
ralhuang <ralhuang@cs.stonybrook.edu>
2021-02-06 16:45:08 -0500

    Finally

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/resume">Resume</Link>
+        </li>
+      </ul>
     </div>
   );
 }
